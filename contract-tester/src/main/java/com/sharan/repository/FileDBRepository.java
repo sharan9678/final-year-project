cb6a2d6631e8bdad22fccbf8bcbe9058caee03cc
sharan9678 <sharan9678@gmail.com>
2023-01-02 18:52:15 +0530

    added file upload codes

---
+++ contract-tester/src/main/java/com/sharan/repository/FileDBRepository.java
@@ -0,0 +1,9 @@
+package com.sharan.repository;
+
+import com.sharan.model.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+}
