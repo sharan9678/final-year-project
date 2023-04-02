6dc04935717c42cfbbea912e3743e1ff5253e6f0
sharan <sharan@gopinathan-2.local>
2023-04-02 19:09:36 +0530

    added required configurations and then the contract testing is set clean

--- contract-tester/src/main/java/com/sharan/repository/FileDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sharan.repository;
-
-import com.sharan.model.FileDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileDBRepository extends JpaRepository<FileDB, String> {
-}
--- contract-tester/src/main/java/com/sharan/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sharan.repository;
-
-import com.sharan.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
